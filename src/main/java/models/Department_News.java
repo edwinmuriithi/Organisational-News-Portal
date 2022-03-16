e8f4ddbb0b0bba041776428cb764b775b8ce39df
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2022-03-16 16:31:19 +0300

    Edit Department_News Class

--- src/main/java/models/Department_News.java
+++ src/main/java/models/Department_News.java
@@ -1,4 +1,25 @@
 package models;
 
-public class Department_News {
+public class Department_News extends News {
+
+    private String news_type;
+    private int department_id;
+    private final String TYPE_OF_NEWS="department";
+
+
+    public Department_News(String title, String description,int department_id, int user_id) {
+        super(title, description, user_id);
+        this.department_id = department_id;
+        this.news_type=TYPE_OF_NEWS;
+    }
+
+    @Override
+    public String getNews_type() {
+        return news_type;
+    }
+
+    @Override
+    public int getDepartment_id() {
+        return department_id;
+    }
 }
