a9c811df206a5308ebc2332e8140bb2fb3719f82
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2022-03-16 16:30:19 +0300

    Edit Department Class

--- src/main/java/models/Departments.java
+++ src/main/java/models/Departments.java
@@ -1,4 +1,42 @@
 package models;
 
 public class Departments {
+
+    private String name;
+    private String description;
+    private int id;
+    private int size;
+
+
+
+    public Departments(String name, String description) {
+        this.name = name;
+        this.description = description;
+        this.size=0;
+    }
+    public String getDescription() {
+        return description;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
 }
