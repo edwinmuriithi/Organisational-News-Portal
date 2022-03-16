59bd372dc49f4fa0d2468ea1af2ca3b2679f3ffa
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2022-03-16 16:40:24 +0300

    Edit Departments Dao

--- src/main/java/dao/DepartmentsDao.java
+++ src/main/java/dao/DepartmentsDao.java
@@ -1,10 +1,23 @@
 package dao;
 
 import models.Departments;
+import models.News;
 import models.Users;
 
 import java.util.List;
 
 public interface DepartmentsDao {
+    //create
+    void add(Departments department);
+    void addUserToDepartment(Users user, Departments department);
+    //read
+
+    List<Departments> getAll();
+    Departments findById(int id);
+    List<Users> getAllUsersInDepartment(int department_id);
+    List<News> getDepartmentNews(int id);
+    //update
+    //delete
+    void clearAll();
 
 }
