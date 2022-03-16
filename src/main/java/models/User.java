ea9df9c446cdbc88308a0e1e53f00271bec74dca
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2022-03-16 16:16:25 +0300

    Edit User Class

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,4 +1,58 @@
 package models;
 
 public class User {
+
+    private int id;
+    private String name;
+    private String position;
+    private int departmentId;
+    private String role;
+
+    public User(int id, String name, String position, int departmentId, String role) {
+        this.id = id;
+        this.name = name;
+        this.position = position;
+        this.departmentId = departmentId;
+        this.role = role;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public int getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(int departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
