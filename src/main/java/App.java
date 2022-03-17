60fb3aaa6b7b2608ce1a1217f86c759ffd72a9f8
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2022-03-17 03:38:39 +0300

    Set up ;ocal db connection

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,25 @@
+import dao.Sql2oDepartmentsDao;
+import dao.Sql2oNewsDao;
+import dao.Sql2oUsersDao;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+
 public class App {
-}
+    public static void main(String[] args) {
+
+        Sql2oNewsDao sql2oNewsDao;
+        Sql2oUsersDao sql2oUserDao;
+        Sql2oDepartmentsDao sql2oDepartmentsDao;
+        Connection conn;
+        Gson gson = new Gson();
+
+        //setup Local connection
+        String connectionString = "jdbc:postgresql://localhost:5432/organisational_news_portal";
+        Sql2o sql2o = new Sql2o(connectionString, "muriithi", "123456");
+
+        sql2oDepartmentsDao = new Sql2oDepartmentsDao(sql2o);
+        sql2oNewsDao = new Sql2oNewsDao(sql2o);
+        sql2oUserDao = new Sql2oUsersDao(sql2o);
+        conn = sql2o.open();
+
+    }
