85a2cc7ab228d5cb6fc28aa9b258db753114102e
Edwin Muriithi <edwin.muriithi@student.moringaschool.com>
2022-03-17 08:38:23 +0300

    Create User and news Test

---
+++ src/test/java/models/NewsTest.java
@@ -0,0 +1,24 @@
+package models;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NewsTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getUser_id() {
+    }
+
+    @Test
+    void getNews_type() {
+    }
+
+    @Test
+    void getDepartment_id() {
+    }
+}
